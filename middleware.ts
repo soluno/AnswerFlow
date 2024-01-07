b33e24ff982b7dff2de161b1537d0c8b8cf5d222
shebll <shebblloll@gmail.com>
2024-01-07 13:43:38 +0200

    protect route with middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,23 @@
-import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("next-auth.session-token");
-  if (!cookie) return NextResponse.redirect(new URL("/login", request.url));
+  if (
+    (!cookie && request.url.includes("setup")) ||
+    request.url.includes("user")
+  ) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  if (
+    (request.url.includes("signup") || request.url.includes("login")) &&
+    cookie
+  ) {
+    return NextResponse.redirect(new URL("/setup", request.url));
+  }
 }
 
 export const config = {
-  matcher: ["/setup/:path*", "/user/:path*"],
+  matcher: ["/setup/:path*", "/user/:path*", "/login/:path*", "/signup/:path*"],
 };
